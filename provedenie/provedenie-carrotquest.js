f7c41c3fdcb6e4e80b86e991fe1b79b122b350e1
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-26 14:29:02 +0200

    Update provedenie-carrotquest.js

--- provedenie/provedenie-carrotquest.js
+++ provedenie/provedenie-carrotquest.js
@@ -15,7 +15,7 @@ carrotquest.connect('14055-2da1eea50c87cb2ecb655ddc78');
 
 var authToken = 'app.14055.46270bd4201d66c944a361bf1bae381f65d2516b149a4808'; // Token from CQ.
 var scriptSrc = 'https://hook.io/evgeniyvorobev/provedenie-carrotquest'; // Hook script src.
-
+var user_id = carrotquest.data.user.id;
 setTimeout(function(){
     if (carrotquest.data.user.id != undefined || carrotquest.data.user.id != '') {
     var user_id = carrotquest.data.user.id;  // Uniq id of CQ user.
@@ -38,7 +38,6 @@ if (a == undefined || a == '') { return }
 var test_result = a; // answer from other script
 form_id = target.id;
 console.log('test-result',test_result);
-var user_id = carrotquest.data.user.id;
 console.log(user_id);
 setTimeout(function() {
     sendSimpleForm()
