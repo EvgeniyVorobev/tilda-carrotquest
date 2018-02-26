6bc0b31398698e48815dc2a0f13ecb550fdd8368
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-26 17:42:36 +0200

    Update provedenie-carrotquest.js

--- provedenie/provedenie-carrotquest.js
+++ provedenie/provedenie-carrotquest.js
@@ -15,10 +15,10 @@ carrotquest.connect('14055-2da1eea50c87cb2ecb655ddc78');
 
 var authToken = 'app.14055.46270bd4201d66c944a361bf1bae381f65d2516b149a4808'; // Token from CQ.
 var scriptSrc = 'https://hook.io/evgeniyvorobev/provedenie-carrotquest'; // Hook script src.
-var user_id = setInterval(function updateUserId(){user_id = carrotquest.data.user.id ;
+var user_id = setInterval(function updateUserId(){user_id = carrotquest.data.user.id ; // Uniq id of CQ user
     user_id.length > 4 ? clearInterval(user_id) : '';
 },1000) ;
-var hidden_name = setInterval(function(){
+var hidden_name = setInterval(function() { // Username from CQ
     if (carrotquest.data.user.id != undefined || carrotquest.data.user.id != '') {
     user_id = carrotquest.data.user.id;  // Uniq id of CQ user.
     $.ajax ({
@@ -34,15 +34,14 @@ var hidden_name = setInterval(function(){
     clearInterval(hidden_name)
 }
 },500)
-// user_id;
 
 
-window.evg = function(a,target){
-if (a == undefined || a == '') { return } // if form answer not added;
-var test_result = a; // answer from other script
-var form_id = target.id;
-console.log('test-result',test_result);
-console.log(user_id);
+/* Function for interview form */
+window.evgCarrot.interviewForm = function(a,target){
+if (a == undefined || a == '') { return } // if form has no answer - abort.
+var test_result = a; // test result from send script.
+var form_id = target.id; // form id.
+
 setTimeout(function() {
     sendSimpleForm()
 },1000)
@@ -62,7 +61,7 @@ function sendSimpleForm(){
     var optionInformation = simpleform.find('option');
     var checkboxInformation = simpleform.find("input[type='checkbox']");
 
-    console.log(formArray);
+    /* Collect information from fields*/
     inputInformation.each(function () {  // Write input data from simpleform, that meet requirements.
         if (this.name != "formname" && this.getAttribute('fieldname') != '' && this.getAttribute('fieldname') != undefined && this.value != '') {  // if has fieldname attribute and value
             allInfo_str[this.getAttribute('fieldname')] = ' '+this.value+' <br>';
@@ -92,16 +91,20 @@ function sendSimpleForm(){
     })
     allInfo_str['Результат Теста'] = ' '+test_result+' <br>'; // Added test results
     allInfo['Результат Теста'] = test_result; // Added test results
+    
+    /* End Collect information */
+
+
     $.each(formArray, filterArray); // Filtering the massive formArray.
     var allInfo_str = JSON.stringify(allInfo_str).replace('{','').replace('}','').replace(/"/g,"").replace(/\<br>,/g,'<br>'); // stringify JSON object for CQ dialogs.
 
-    console.log(JSON.stringify(allInfo_str));
-    console.log('allInfo_str ',allInfo_str);
-    console.log('allInfo ',allInfo)
+    // console.log(JSON.stringify(allInfo_str));
+    // console.log('allInfo_str ',allInfo_str);
+    // console.log('allInfo ',allInfo)
 
-    // Filter $form, and set value if there is.
+    // Filter form, and set value if there is.
     function filterArray() {    
-        if (this.name == 'formname') {
+    if (this.name == 'formname') {
         formname = this.value   ; // hidden form name from hidden input field.
     }
     if (this.name == 'name' || this.name == 'Name') { // search for name.
@@ -118,7 +121,7 @@ function sendSimpleForm(){
     } 
 }
 
-    // ____----___---___---____-----___----___----- ____-----___-----___-----___-----___----//
+// ____----___---___---____-----___----___----- ____-----___-----___-----___-----___----//
 
     /* Send Data to CQ */
     function sendToCarrot(){  
@@ -149,8 +152,7 @@ function sendSimpleForm(){
     if (formname == undefined) { // if $form didn't have formname input-field.
         /*Отправка данных в lead*/
     carrotquest.track('Заполнил форму без имени', allInfo);
-
-    /* Отправка данных в диалоги через hook */
+    /* Send to Hook -> CQ dialogs */
     $.ajax({
         type: 'POST',
         url: scriptSrc,
@@ -173,15 +175,11 @@ sendToCarrot();
 /* End of Simpleform  */
 
 
-
 /* Function whitch run after submit event - Tilda Form (not payments form's). */
 $(document).ready(function(){
-window.mySuccessFunction = function($form){
-
-    console.log('функция работает');
-    console.log($form);
-    var text,text1,text2,text3,text4,text5,text6,
-    checkbox,checkbox1,checkbox2,checkbox3,checkbox4,checkbox5,checkbox6,
+    window.mySuccessFunction = function($form){
+        var text,text1,text2,text3,text4,text5,text6,
+        checkbox,checkbox1,checkbox2,checkbox3,checkbox4,checkbox5,checkbox6,
     formname,name,familyname,email,phone,year,formName,price; // Identify variables for transfering too Hook e.t.c
     var formArray = $form.serializeArray(); // Massive with data from $form.
     var allInfo = {};  // Object with all info for transfering. 
@@ -191,7 +189,6 @@ window.mySuccessFunction = function($form){
     var selectInformation = $($form).find('select');
     var optionInformation = $($form).find('option');
     var checkboxInformation = $($form).find("input[type='checkbox']");
-
 
     inputInformation.each(function () {  // Write input data from $form, that meet requirements.
         if (this.name != 'tildaspec-projectid' && this.name != 'tildaspec-pageid' && this.name != 'formservices[]' 
@@ -260,8 +257,8 @@ window.mySuccessFunction = function($form){
 
  // ____----___---___---____-----___----___----- ____-----___-----___-----___-----___----//
 
- /* Send Data to CQ */
- function sendToCarrot(){  
+    /* Send Data to CQ */
+    function sendToCarrot(){  
     if ( name != undefined || name != '') { // Update CQ user's name.
         carrotquest.identify({
             '$name': name
@@ -285,12 +282,11 @@ window.mySuccessFunction = function($form){
         })
     }
 
-
      if (formname == undefined) { // if $form didn't have formname input-field.
-        /*Отправка данных в lead*/
+        /* Send to CQ Lead */
     carrotquest.track('Заполнил форму без имени', allInfo);
 
-    /* Отправка данных в диалоги через hook */
+    /* Send to hook -> CQ dialogs.*/
     $.ajax({
         type: 'POST',
         url: scriptSrc,
