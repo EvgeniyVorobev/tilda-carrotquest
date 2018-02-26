616ce87cc83fcde2df013eb3d89405cbf64ec126
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-26 07:41:06 +0200

    Update provedenie-carrotquest.js

--- provedenie/provedenie-carrotquest.js
+++ provedenie/provedenie-carrotquest.js
@@ -14,33 +14,14 @@ carrotquest.connect('14055-2da1eea50c87cb2ecb655ddc78');
 <!-- CarrotQuest END -->
 
 
-var user_id;
-    var authToken = 'app.14055.46270bd4201d66c944a361bf1bae381f65d2516b149a4808'; // Token from CQ.
-    var scriptSrc = 'https://hook.io/evgeniyvorobev/provedenie-carrotquest'; // Hook script src.
-
-    setTimeout(function(){
-        if (carrotquest.data.user.id != undefined || carrotquest.data.user.id != '') {
-    user_id = carrotquest.data.user.id;  // Uniq id of CQ user.
-    $.ajax ({
-        type: 'GET',
-        url: 'https://api.carrotquest.io/v1/users/'+user_id,
-        data: { 
-            'auth_token': authToken
-        },
-        success: function(data) {
-            $('body').append('<div id="carrotUsername" hidden>'+data.data.props.$name+'</div>')
-        } 
-    }) ;
-}
-},500)
-
-
-
-    window.evg = function(a,target){
-        if (a == undefined || a == '') { return }
+window.evg = function(a,target){
+    if (a == undefined || a == '') { return }
     var test_result = a; // answer from other script
 form_id = target.id;
-
+console.log(form_id);
+console.log(form_id);
+console.log($(target).is('[simpleform]') );
+console.log('test-result',test_result);
 var user_id;
     var authToken = 'app.14055.46270bd4201d66c944a361bf1bae381f65d2516b149a4808'; // Token from CQ.
     var scriptSrc = 'https://hook.io/evgeniyvorobev/provedenie-carrotquest'; // Hook script src.
@@ -61,12 +42,9 @@ var user_id;
 }
 },500)
 
-    console.log(form_id);
-    console.log(form_id);
-    console.log($(target).is('[simpleform]') );
-    console.log('test-result',test_result);
-
-    sendSimpleForm()
+    setTimeout(function() {
+        sendSimpleForm()
+    },1000)
 
     /*Catch information from forms with attr simpleform fields. !*/
     function sendSimpleForm(){
@@ -348,6 +326,5 @@ sendToCarrot();
 
 
 };
-
 
   // evg() // Execude script
