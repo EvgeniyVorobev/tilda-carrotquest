66a6278284bef3d6a4eaf69a5b2b6bca8dbdfa7c
Evgeniy Vorobev <evggordadym@gmail.com>
2018-04-02 12:28:41 +0300

    Update locallocal.ru-carrotquest.js

--- locallocal.ru/locallocal.ru-carrotquest.js
+++ locallocal.ru/locallocal.ru-carrotquest.js
@@ -455,7 +455,7 @@ function sendSimpleForm(){ // Catch information from form.
         });
         }
 
-    if (formname != undefined && formname != 'nosend') { // if form got name="formname" .
+    if (formname != undefined) { // if form got name="formname" .
         carrotquest.track('Заполнил форму '+formname, allInfo); // send info to CQ leads.
         $.ajax({ // send to hook.io -> CQ dialogs.
             type: 'POST',
@@ -599,7 +599,7 @@ $(document).ready(function(){
         });
     }
 
-    if (formname != undefined) { // if $form got name="formname" 
+    if (formname != undefined && formname != 'nosend') { // if $form got name="formname" 
         carrotquest.track('Заполнил форму '+formname, allInfo); // send info to CQ Leads.
         $.ajax({ // send to hook.io -> CQ dialogs
             type: 'POST',
