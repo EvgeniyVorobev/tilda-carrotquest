c0de1853f468c32171e0d8c6bb0741ea773ca83d
Evgeniy Vorobev <evggordadym@gmail.com>
2018-06-01 08:33:38 +0300

    Update svoemedia-carrotquest.js

--- svoemedia/svoemedia-carrotquest.js
+++ svoemedia/svoemedia-carrotquest.js
@@ -54,7 +54,7 @@ $(function(){
 	var href_name = '#'+this.href.split('/')[this.href.split('/').length-1].replace(/.*#/gi,""); // popup href name;
 	console.log(href_name);
 	if ($("div[data-tooltip-hook="+"'"+href_name+"'"+"]")) {
-	var popup_formname = $("div[data-tooltip-hook="+"'"+href_name+"'"+"]").find('input[name="formname"]')[0].value; // find popup formname VALUE
+	var popup_formname = $("div[data-tooltip-hook="+"'"+href_name+"'"+"]").find('input[name="formname"]')[0].value || $("div[data-tooltip-hook="+"'"+href_name+"'"+"]")[0] ; // find popup formname VALUE
 	}
 	carrotquest.track('Перешел к заполнению формы '+'[ '+popup_formname+' ]'); // send info to CQ leads.
 })
