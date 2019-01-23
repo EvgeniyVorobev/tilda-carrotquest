51f97d243027a114fa1817062650b216d94cc413
Evgeniy Vorobev <evggordadym@gmail.com>
2019-01-23 18:12:11 +0200

    Update popupgrade-carrotquest.js

--- popupgrade.ru/popupgrade-carrotquest.js
+++ popupgrade.ru/popupgrade-carrotquest.js
@@ -5,22 +5,8 @@ var authToken = 'app.23410.ec50351cb76b188da9aad7e66156672d336a6ada4fc5efd2'; //
 var scriptSrc = 'https://hook.io/evgeniyvorobev/popupgrade-carrotquest'; // Hook script src.
 /* ..end [Preference] */
 
-
 /* Script from CQ Preference */
-<!-- CarrotQuest BEGIN -->
-(function(){
-    function Build(name, args){return function(){window.carrotquestasync.push(name, arguments);} }
-    if (typeof carrotquest === 'undefined') {
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
-        s.src = '//cdn.carrotquest.io/api.min.js';
-        var x = document.getElementsByTagName('head')[0]; x.appendChild(s);
-        window.carrotquest = {}; window.carrotquestasync = []; carrotquest.settings = {};
-        var m = ['connect', 'track', 'identify', 'auth', 'open', 'onReady', 'addCallback', 'removeCallback', 'trackMessageInteraction'];
-        for (var i = 0; i < m.length; i++) carrotquest[m[i]] = Build(m[i]);
-    }
-})();
-carrotquest.connect('7133-b8bd2e320f9d0a7743362aae6a4');
-<!-- CarrotQuest END -->
+!function(){function t(t,e){return function(){window.carrotquestasync.push(t,arguments)}}if("undefined"==typeof carrotquest){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//cdn.carrotquest.io/api.min.js",document.getElementsByTagName("head")[0].appendChild(e),window.carrotquest={},window.carrotquestasync=[],carrotquest.settings={};for(var n=["connect","track","identify","auth","oth","onReady","addCallback","removeCallback","trackMessageInteraction"],a=0;a<n.length;a++)carrotquest[n[a]]=t(n[a])}}(),carrotquest.connect("23410-993634aa62aa420eeb856a9bf2");
 /* ..end [Script from CQ Preference] */
 
 if (typeof(window.Tilda) == 'object') { // If site use Tilda.cc
